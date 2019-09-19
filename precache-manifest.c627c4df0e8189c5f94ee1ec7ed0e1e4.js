6217858e55b4337b074dcd7cf4e9d4bafdad151c
Aliakwe <aliakwe@yahoo.com>
2019-09-20 00:20:25 +0100

    Updates

---
+++ precache-manifest.c627c4df0e8189c5f94ee1ec7ed0e1e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21b6a5b1ad368735ac6270791732e17",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1da6182127cce31823d0",
+    "url": "/todolist/static/css/main.fd093e9b.chunk.css"
+  },
+  {
+    "revision": "4b6ef0e8ca2edf826b77",
+    "url": "/todolist/static/js/2.41f69250.chunk.js"
+  },
+  {
+    "revision": "1da6182127cce31823d0",
+    "url": "/todolist/static/js/main.c9d7c984.chunk.js"
+  },
+  {
+    "revision": "e657250678c4b7d29af2",
+    "url": "/todolist/static/js/runtime~main.aa01b21e.js"
+  }
+]);
